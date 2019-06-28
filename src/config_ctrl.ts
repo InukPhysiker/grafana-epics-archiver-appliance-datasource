e25ac2ca142a04b701a491002972bb6e1f14cf31
InukPhysiker <InukPhysiker@gmail.com>
2019-06-28 09:11:14 -0600

    add getSuggestUrls and getApplianceInfo

--- src/config_ctrl.ts
+++ src/config_ctrl.ts
@@ -3,7 +3,35 @@
 export class EPICSArchAppConfigCtrl {
   static templateUrl = 'partials/config.html';
   current: any;
+  suggestUrl: string;
+  datasources: any;
 
-  constructor($scope) {
+  constructor($scope, private backendSrv) {
+    this.suggestUrl = "http://vm-archiver-02:17665";
+    this.current.jsonData.identity = this.current.jsonData.identity || '';
+    this.current.jsonData.engineURL = this.current.jsonData.engineURL || '';
+    this.current.jsonData.retrievalURL = this.current.jsonData.retrievalURL || '';
+    this.current.jsonData.dataRetrievalURL = this.current.jsonData.dataRetrievalURL || '';
+  }
+  getSuggestUrls = () => {
+    return [this.suggestUrl];
+  }
+
+  getApplianceInfo() {
+    this.backendSrv
+      .datasourceRequest({
+        url: `api/datasources/proxy/${this.current.id}/archiverappliance/mgmt/bpl/getApplianceInfo`,
+        method: 'GET'
+      })
+      .then(res => {
+        this.current.jsonData.identity = res.data.identity;
+        this.current.jsonData.engineURL = res.data.engineURL;
+        this.current.jsonData.retrievalURL = res.data.retrievalURL;
+        this.current.jsonData.dataRetrievalURL = res.data.dataRetrievalURL;
+        return;
+      })
+      .catch(err => {
+        console.log(err);
+      });
   }
 }
