1b99330693a5ad9e0f1a47df0798d1e0747cd018
InukPhysiker <InukPhysiker@gmail.com>
2020-06-05 10:43:35 -0600

    Fix check for pv not specified yet

--- src/datasource.ts
+++ src/datasource.ts
@@ -27,7 +27,7 @@ export default class EPICSArchAppDatasource {
 
         const pvname = options.targets[0].pvname;
 
-        if (pvname == '' || pvname == undefined) {
+        if (pvname == '' || pvname == '-- pvname --' || pvname == undefined) {
             return this.$q.when({ data: [] });
         }
 
